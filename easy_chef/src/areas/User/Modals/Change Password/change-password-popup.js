77b0c8325190520f473d0f437da0232316a0c85b
Stas.V <stan@web4you.ca>
2023-04-10 19:39:13 -0400

    fixed styling of password change

--- easy_chef/src/areas/User/Modals/Change Password/change-password-popup.js
+++ easy_chef/src/areas/User/Modals/Change Password/change-password-popup.js
@@ -62,19 +62,9 @@ const ChangePasswordModal = ({show, onClose, profileEmail}) => {
     return (
         <>
             <Modal style={{width: 385}} isOpen={show} toggle={close} centered>
-                <ModalHeader toggle={close}></ModalHeader>
+                <ModalHeader toggle={close}>Password Change</ModalHeader>
                 <ModalBody>
                     <div className="card-body">
-                        <div className="app-brand justify-content-center mb-2 mt-n4">
-                            <a href="index.html" className="app-brand-link gap-2">
-                                <span className="app-brand-logo demo">
-                                <img alt='logo' src={logo} style={{ maxHeight: "50px" }}></img>
-                            </span>
-                                <span className="app-brand-text demo text-body fw-bold ms-1">EasyChef</span>
-                            </a>
-                        </div>
-                        <h4 className="mb-1 pt-2">Change Password 🔒</h4>
-                        <p className="mb-4">for <span className="fw-bold">{profileEmail}</span></p>
                         <Form onSubmit={handleSubmit(onSubmit)}>
                             <div className='mb-1'>
                                 <Label className='form-label' for='old_password'>
