acd0277a97450cb5928792f144fd998a986bfff1
Stas.V <stan@web4you.ca>
2023-04-15 21:27:43 -0400

    added start up and run scripts

---
+++ run.sh
@@ -0,0 +1,4 @@
+cd ./backend
+python3.10 manage.py runserver
+cd ../easy_chef
+npm start
--- startup.sh
+++ startup.sh
@@ -1,4 +1,7 @@
+cd ./backend
 python3.10 -m venv env
 python3.10 -m pip install django
 python3.10 -m pip install -r requirements.txt
 python3.10 manage.py migrate
+cd ../easy_chef
+npm install
